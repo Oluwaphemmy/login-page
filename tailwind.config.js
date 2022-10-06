45a35b8a3d8583045902a9fc19013a27fbdb41ce
Oluwaphemmy <ojokofemi@gmail.com>
2022-10-06 18:50:11 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "grad-texture": "url('/img/gradient.png')",
+      },
+    },
+  },
+  plugins: [],
+};
