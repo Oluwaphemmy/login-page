49da3059dede45d21a1e9ff1f55f5aa5b20f4110
Oluwaphemmy <ojokofemi@gmail.com>
2022-10-08 09:37:52 +0100

    config color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
       backgroundImage: {
         "grad-texture": "url('/img/gradient.png')",
       },
+      colors: {
+        button: "#046586",
+        login: "#161843",
+        placeholder: "#717a90",
+        sign: "#0465",
+        or: "#838a9e",
+        forget: "#0c6787",
+      },
+      fontFamily: {
+        mulish: ['"Mulish"', sans - serif],
+        rokit: [' "Rokkitt" ', serif],
+      },
     },
   },
   plugins: [],
